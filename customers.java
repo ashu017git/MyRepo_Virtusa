65bafb1bbcde9b55ba322f94de76f3edf76bb15b
Kaverigithub <77496696+Kaverigithub@users.noreply.github.com>
2022-03-07 19:14:21 +0530

    Add files via upload

---
+++ customers.java
@@ -0,0 +1,86 @@
+package com.virtusa.beans;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customers2")
+public class customers {
+
+	 @Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		private long id;
+		@Column(length=10)
+		private int product_id,phone; 
+		private String name, email, productname,address,username, password;
+		
+		
+		@OneToMany(mappedBy="pro",cascade=CascadeType.ALL)
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+	
+
+	public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+}
+
+	
+
+	
